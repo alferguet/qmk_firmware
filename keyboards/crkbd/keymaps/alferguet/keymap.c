15e22be1f1dbeb9c51f005ebaf5b8319c74f7f55
alferguet <afernandezh@protonmail.com>
2021-01-12 21:19:44 +0100

    Misc stuff

--- keyboards/crkbd/keymaps/alferguet/keymap.c
+++ keyboards/crkbd/keymaps/alferguet/keymap.c
@@ -31,14 +31,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_LBRC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_RBRC,\
 LCTL_T(KC_GRV),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, RCTL_T(KC_QUOT),\
 KC_LSPO,    ALT_T(KC_Z),    RALT_T(KC_X),    KC_C,    KC_V,    KC_B,           KC_N,    KC_M, KC_COMM,  RALT_T(KC_DOT), LALT_T(KC_SLSH), KC_RSPC,\
-													GUI_T(KC_TAB), LT(1, KC_ESC),  SFT_T(KC_SPC),     RSFT_T(KC_BSPC),  LT(2, KC_ENT),  GUI_T(KC_MINS) \
+													GUI_T(KC_TAB), LT(1, KC_ESC), SFT_T(KC_SPC) ,    RSFT_T(KC_BSPC), LT(2, KC_ENT),  GUI_T(KC_MINS) \
 
 ),
 
 	[_LOWER] = LAYOUT_split_3x6_3( \
 LGUI(KC_6), LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
 			LGUI(KC_ENT), KC_MS_BTN1, KC_MS_BTN2, KC_MS_BTN3, KC_HOME, KC_PGUP,   KC_LEFT, KC_DOWN, KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,\
-			XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_END, KC_PGDN,                  KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, XXXXXXX, XXXXXXX,\
+			KC_CAPS, LCTL(KC_Z), LCTL(KC_Y), KC_APP, KC_END, KC_PGDN,                  KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT, XXXXXXX, XXXXXXX,\
 																					XXXXXXX,   XXXXXXX,  XXXXXXX,     XXXXXXX,  KC_DEL , MO(3) \
 	),
 
