5a36446975abe4ca287173ce6800fc964f9e9bb0
alferguet <afernandezh@protonmail.com>
2021-06-18 15:46:54 +0200

    gaeming layer

--- keyboards/crkbd/keymaps/alferguet/keymap.c
+++ keyboards/crkbd/keymaps/alferguet/keymap.c
@@ -1,11 +1,5 @@
 #include QMK_KEYBOARD_H
 
-
-#ifdef RGBLIGHT_ENABLE
-//Following line allows macro to read current RGB settings
-extern rgblight_config_t rgblight_config;
-#endif
-
 extern uint8_t is_master;
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
@@ -16,6 +10,7 @@ extern uint8_t is_master;
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 3
+#define _GAMING 4
 
 enum custom_keycodes {
 	QWERTY = SAFE_RANGE,
@@ -36,7 +31,7 @@ KC_LSPO,    ALT_T(KC_Z),    RALT_T(KC_X),    KC_C,    KC_V,    KC_B,       KC_N,
 ),
 
 	[_LOWER] = LAYOUT_split_3x6_3( \
-LGUI(KC_6), LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),     KC_HOME, KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+LGUI(KC_6), LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),     KC_HOME, KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX, TO(4),\
 			XXXXXXX, LGUI(KC_ENT), LGUI(KC_SPC), XXXXXXX, XXXXXXX, XXXXXXX,       KC_LEFT, KC_DOWN, KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,\
 			KC_CAPS, LCTL(KC_Z), LCTL(KC_Y), LCTL(KC_C), LCTL(KC_V), XXXXXXX,     KC_END, KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
 																					XXXXXXX,   XXXXXXX,  XXXXXXX,     XXXXXXX,  KC_DEL , MO(3) \
@@ -59,140 +54,13 @@ LGUI(KC_6), LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),     KC_H
 	//|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
 																			XXXXXXX,   XXXXXXX,  XXXXXXX,     XXXXXXX,   XXXXXXX, XXXXXXX \
 																			//`--------------------------'  `--------------------------'
-	)
+	),
+[_GAMING] = LAYOUT_split_3x6_3( \
+	KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  TO(0),\
+KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
+KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSPC,\
+													KC_TAB, LT(1, KC_ESC), KC_SPC ,    KC_BSPC, LT(2,KC_ENT),  GUI_T(KC_MINS) \
+
+)
+
 };
-
-int RGB_current_mode;
-
-void persistent_default_layer_set(uint16_t default_layer) {
-	eeconfig_update_default_layer(default_layer);
-	default_layer_set(default_layer);
-}
-
-// Setting ADJUST layer RGB back to default
-void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-	if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
-		layer_on(layer3);
-	} else {
-		layer_off(layer3);
-	}
-}
-
-void matrix_init_user(void) {
-		#ifdef RGBLIGHT_ENABLE
-		RGB_current_mode = rgblight_config.mode;
-		#endif
-		//SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
-		#ifdef SSD1306OLED
-		iota_gfx_init(!has_usb());   // turns on the display
-		#endif
-}
-
-//SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
-#ifdef SSD1306OLED
-
-// When add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
-const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
-
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
-
-void matrix_scan_user(void) {
-	iota_gfx_task();
-}
-
-void matrix_render_user(struct CharacterMatrix *matrix) {
-	if (is_master) {
-		// If you want to change the display of OLED, you need to change here
-		matrix_write_ln(matrix, read_layer_state());
-		matrix_write_ln(matrix, read_keylog());
-		//matrix_write_ln(matrix, read_keylogs());
-		//matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
-		//matrix_write_ln(matrix, read_host_led_state());
-		//matrix_write_ln(matrix, read_timelog());
-	} else {
-		matrix_write(matrix, read_logo());
-	}
-}
-
-void matrix_update(struct CharacterMatrix *dest, const struct CharacterMatrix *source) {
-	if (memcmp(dest->display, source->display, sizeof(dest->display))) {
-		memcpy(dest->display, source->display, sizeof(dest->display));
-		dest->dirty = true;
-	}
-}
-
-void iota_gfx_task_user(void) {
-	struct CharacterMatrix matrix;
-	matrix_clear(&matrix);
-	matrix_render_user(&matrix);
-	matrix_update(&display, &matrix);
-}
-#endif//SSD1306OLED
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	if (record->event.pressed) {
-		#ifdef SSD1306OLED
-		set_keylog(keycode, record);
-#endif
-		// set_timelog();
-	}
-
-	switch (keycode) {
-		case QWERTY:
-			if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_QWERTY);
-			}
-      return false;
-		case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      }
-			return false;
-		case RAISE:
-			if (record->event.pressed) {
-				layer_on(_RAISE);
-				update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-			} else {
-				layer_off(_RAISE);
-				update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-			}
-			return false;
-		case ADJUST:
-      	if (record->event.pressed) {
-					layer_on(_ADJUST);
-				} else {
-					layer_off(_ADJUST);
-				}
-      	return false;
-		case RGB_MOD:
-			#ifdef RGBLIGHT_ENABLE
-			if (record->event.pressed) {
-					rgblight_mode(RGB_current_mode);
-					rgblight_step();
-					RGB_current_mode = rgblight_config.mode;
-			}
-			#endif
-			return false;
-		case RGBRST:
-			#ifdef RGBLIGHT_ENABLE
-			if (record->event.pressed) {
-				eeconfig_update_rgblight_default();
-				rgblight_enable();
-				RGB_current_mode = rgblight_config.mode;
-			}
-			#endif
-			break;
-	}
-	return true;
-}
