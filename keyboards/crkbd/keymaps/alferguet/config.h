4c4a5935af4d765f0aa5bcecbb656931955f187e
alferguet <afernandezh@protonmail.com>
2021-07-17 21:44:31 +0200

    Updated shortcuts

--- keyboards/crkbd/keymaps/alferguet/config.h
+++ keyboards/crkbd/keymaps/alferguet/config.h
@@ -25,8 +25,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Select hand configuration */
 
  
-#define MASTER_LEFT
-// #define MASTER_RIGHT
+
+// #define MASTER_LEFT
+#define MASTER_RIGHT
 // #define EE_HANDS
 
 
@@ -36,4 +37,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define IGNORE_MOD_TAP_INTERRUPT
 
-#define TAPPING_TERM 150
+#define TAPPING_TERM 145
--- keyboards/crkbd/keymaps/alferguet/keymap.c
+++ keyboards/crkbd/keymaps/alferguet/keymap.c
@@ -23,25 +23,24 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT_split_3x6_3( \
-	KC_LBRC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_RBRC,\
-LCTL_T(KC_GRV),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, RCTL_T(KC_QUOT),\
-KC_LSPO,    ALT_T(KC_Z),    RALT_T(KC_X),    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  RALT_T(KC_DOT), LALT_T(KC_SLSH), KC_RSPC,\
-													GUI_T(KC_TAB), LT(1, KC_ESC), SFT_T(KC_SPC) ,    RSFT_T(KC_BSPC), LT(2, KC_ENT),  GUI_T(KC_MINS) \
-
+	KC_LBRC,    		KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  RALT_T(KC_RBRC),\
+  LCTL_T(KC_GRV), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, RCTL_T(KC_QUOT),\
+  KC_LSPO, 			  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSPC,\
+													GUI_T(KC_TAB), LT(1, KC_ESC), KC_SPC ,    KC_BSPC, LT(2, KC_ENT),  LALT_T(KC_MINS) \
 ),
 
 	[_LOWER] = LAYOUT_split_3x6_3( \
-LGUI(KC_6), LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),     KC_HOME, KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX, TO(4),\
-			XXXXXXX, LGUI(KC_ENT), LGUI(KC_SPC), XXXXXXX, XXXXXXX, XXXXXXX,       KC_LEFT, KC_DOWN, KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,\
-			KC_CAPS, LCTL(KC_Z), LCTL(KC_Y), LCTL(KC_C), LCTL(KC_V), XXXXXXX,     KC_END, KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-																					XXXXXXX,   XXXXXXX,  XXXXXXX,     XXXXXXX,  KC_DEL , MO(3) \
+	LGUI(KC_6), LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),     KC_HOME, KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX, TO(4),\
+  LGUI(KC_ENT), LGUI(KC_SPC), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_LEFT, KC_DOWN, KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,\
+	KC_CAPS, LCTL(KC_Z), LCTL(KC_Y), LCTL(KC_C), LCTL(KC_V), XXXXXXX,     KC_END, KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+																					XXXXXXX,   XXXXXXX,  XXXXXXX,     KC_DEL, KC_LSFT  , MO(3) \
 	),
 
 	[_RAISE] = LAYOUT_split_3x6_3( \
 		KC_PIPE, KC_EXLM, KC_AT, KC_HASH, KC_DLR,  KC_PERC,                  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS, \
-		KC_MINS,  KC_1,    KC_2,  KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
+		KC_PLUS,  KC_1,    KC_2,  KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
 		KC_F12,  KC_F1,   KC_F2, KC_F3,   KC_F4,   KC_F5,                    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
-																					MO(3), KC_DEL, _______, _______, _______, _______ \
+																					MO(3), XXXXXXX, KC_COLON, _______, _______, _______ \
 		),
 
 	[_ADJUST] = LAYOUT_split_3x6_3( \
@@ -62,5 +61,4 @@ KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_CO
 													KC_TAB, LT(1, KC_ESC), KC_SPC ,    KC_BSPC, LT(2,KC_ENT),  GUI_T(KC_MINS) \
 
 )
-
 };
